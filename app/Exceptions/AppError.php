9c53534f74ccd8b4ee33bb701fea9c19e6f2a067
João Pedro Silva Cunha <jpcunha5@gmail.com>
2025-06-09 02:02:40 -0300

    :construction_worker: Preparando para testes unitários

--- app/Exceptions/AppError.php
+++ app/Exceptions/AppError.php
@@ -3,26 +3,41 @@
 namespace App\Exceptions;
 
 use Exception;
-use Throwable;
+use Throwable; // Importe Throwable para a interface de exceção
 
 class AppError extends Exception
 {
-    protected $message, $httpCode, $shouldLog;
+    /**
+     * O código HTTP da exceção.
+     * Geralmente mapeado para o status HTTP da resposta.
+     *
+     * @var int
+     */
+    protected $code;
 
-    public function __construct(string $message, int $httpCode = 400, bool $shouldLog = true)
+    /**
+     * Construtor da exceção personalizada AppError.
+     *
+     * @param string $message A mensagem de erro.
+     * @param int $code O código da exceção (geralmente um código HTTP como 400, 404, 500).
+     * @param ?Throwable $previous A exceção anterior usada para o encadeamento de exceções.
+     */
+    public function __construct(string $message = "", int $code = 0, ?Throwable $previous = null)
     {
-        $this->message = $message;
-        $this->httpCode = $httpCode;
-        $this->shouldLog = $shouldLog;
+
+        parent::__construct($message, $code, $previous);
+        $this->code = $code;
     }
 
+    /**
+     * Retorna o código HTTP associado a esta exceção.
+     * Embora getCode() já exista na classe pai, pode ser útil ter um nome mais descritivo
+     * se você estiver tratando-o especificamente como um código HTTP.
+     *
+     * @return int
+     */
     public function getHttpCode(): int
     {
-        return $this->httpCode;
-    }
-
-    public function getShouldLog(): int
-    {
-        return $this->shouldLog;
+        return $this->code;
     }
 }
